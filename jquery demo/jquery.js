e599a45c1ad2701527770b744487d8c1ae9032cd
singt551 <singt551@newschool.edu>
2022-11-03 23:00:25 -0400

    jquery

---
+++ jquery demo/jquery.js
@@ -0,0 +1,21 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[hello people of earth! you have 5 minutes to collect you belongings and run because the aliens have began their decent onto planet earth and are ready to take over! so RUNNN!]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#pineapple").draggable(); })
+
+$(document).ready(function() {$("#alien1").draggable(); })
+
+$(document).ready(function() {$("#alien2").draggable(); })
+
+$(document).ready(function() {$("#alien3").draggable(); })
+
+
