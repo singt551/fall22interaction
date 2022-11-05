a0d8f75e8624c15cfd0962f29f5b23e9b32070b0
singt551 <singt551@newschool.edu>
2022-11-05 00:23:48 -0400

    entry 8 edits
    - draggable needed

---
+++ entries/entry8/site.js
@@ -0,0 +1,17 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[Plate this meal as you wish!]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#wrap").draggable(); })
+
+$(document).ready(function() {$("#cauliflower1").draggable(); })
+
+$(document).ready(function() {$("#cauliflower2").draggable(); })
+
+$(document).ready(function() {$("#cauliflower3").draggable(); })
+
+$(document).ready(function() {$("#salad").draggable(); })
+
+$(document).ready(function() {$("#barbecue").draggable(); })
+
+
