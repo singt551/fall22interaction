45da3d4c083fb94c8409bf4964ca50fdb488b61c
singt551 <singt551@newschool.edu>
2022-11-14 23:20:36 -0500

    entry 9 edited

---
+++ entries/entry9/jquery.js
@@ -0,0 +1,21 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[Entertain yourself with a game of tic-tac-toe!]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#burger").draggable(); })
+
+$(document).ready(function() {$("#burger1").draggable(); })
+
+$(document).ready(function() {$("#burger2").draggable(); })
+
+$(document).ready(function() {$("#burger3").draggable(); })
+
+$(document).ready(function() {$("#burger4").draggable(); })
+
+$(document).ready(function() {$("#fries").draggable(); })
+
+$(document).ready(function() {$("#fries1").draggable(); })
+
+$(document).ready(function() {$("#fries2").draggable(); })
+
+$(document).ready(function() {$("#fries3").draggable(); })
